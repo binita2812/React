fff0fae1aaa0dc8838dddc8c0afc0742c5edafa6
binita2812 <chattaraj.binita02@gmail.com>
2024-03-18 22:50:43 +0530

    qr

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Accordion from './components/accordion';
+import QRCodeGenerator from './components/accordion/qr-code-generator';
 
 function App() {
   return (
     <div className="App">
       <Accordion/>
+      <QRCodeGenerator/>
     </div>
   );
 }
